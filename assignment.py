4f7ed8c74a4e4137c718f41a2a5ad65a9f097e77
HanhDoan57 <tdoan2@miners.utep.edu>
2026-05-08 15:34:51 -0600

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -5,5 +5,25 @@ import matplotlib.pyplot as plt
 # please install them using pip.
 
 filename = 'Flight_Delays_2018.csv'
+df = pd.read_csv(filename)
 
-# ARR_DELAY is the column name that should be used as dependent variable (Y).
+df_filtered = df[df['OP_CARRIER'] == 'DL']
+df_filtered = df_filtered.dropna(subset=['ARR_DELAY', 'DEP_DELAY', 'DISTANCE'])
+print(df_filtered[['ARR_DELAY', 'DEP_DELAY', 'DISTANCE']].describe())
+print(df_filtered[['ARR_DELAY', 'DEP_DELAY', 'DISTANCE']].corr())
+
+# Prepare the data for OLS regression
+#dependency variable
+Y = df_filtered["ARR_DELAY"]
+#independent variables
+X = df_filtered[["DEP_DELAY", "DISTANCE"]]
+X = sm.add_constant(X)
+
+model = sm.OLS(Y, X).fit()
+print(model.summary())
+
+plt.scatter(df_filtered["DEP_DELAY"], df_filtered["ARR_DELAY"], alpha=0.5)
+plt.xlabel("Departure delay (minutes)")
+plt.ylabel("Arrival delay (minutes)")
+plt.title("Arrival vs Departure Delay")
+plt.show()
